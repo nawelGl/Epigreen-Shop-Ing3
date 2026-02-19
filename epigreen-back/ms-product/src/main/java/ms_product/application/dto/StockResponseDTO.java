29c80718dd81ae60e7970894e8aa842fc9a4d3af
nawelGl <ghazal.nawel@gmail.com>
2026-02-19 22:18:46 +0100

    add dtos

---
+++ epigreen-back/ms-product/src/main/java/ms_product/application/dto/StockResponseDTO.java
@@ -0,0 +1,17 @@
+package ms_product.application.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class StockResponseDTO {
+    private Integer id;
+    private Integer productId;
+    private String sizeLabel;
+    private Integer quantity;
+}
