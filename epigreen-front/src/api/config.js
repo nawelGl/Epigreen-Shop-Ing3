5cfa5b17fa6e322a8e1a334c124dab9b9c6ace53
seulgi <seulgi-shin@etu.u-pec.fr>
2026-02-20 23:08:34 +0100

    afficahge products

--- epigreen-front/src/api/config.js
+++ epigreen-front/src/api/config.js
@@ -2,6 +2,8 @@
 export const CONFIG = {
     API: {
         CUSTOMER: "http://localhost:8081/api/customers",
-        CART: "http://localhost:8083/api/cart"
+        CART: "http://localhost:8083/api/cart",
+        PRODUCT: "http://localhost:8082/api/products",
+        TRACKER: "http://localhost:3000/api/track"
     }
 };
