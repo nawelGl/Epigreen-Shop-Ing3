c36b2b2bb6f6bc632d17590aa948947df65617c9
seulgi <seulgi-shin@etu.u-pec.fr>
2026-02-27 09:46:28 +0100

    add diagramme de séquence

--- epigreen-front/src/api/config.js
+++ epigreen-front/src/api/config.js
@@ -6,7 +6,7 @@ export const CONFIG = {
         CART: "http://172.31.252.28:8083/api/cart",
         EVENTTRACKER: "http://172.31.250.47:4000/api/track/events",
         ORDER: "http://172.31.252.28:8084/api/orders",
-        TRACKER: "http://localhost:3000/api/track",
-        DELIVERY: "http://localhost:8087/api/delivery"
+        TRACKER: "http://l172.31.252.12:3000/api/track",
+        DELIVERY: "http://172.31.252.74:8087/api/delivery"
     }
 };
